993b4aedce3aa29c889ddbd08005db418cf4b839
Misa <milosursulovic2696@gmail.com>
2023-01-27 20:19:50 +0100

    Add photo dto

---
+++ app/src/main/java/com/example/imagesearch/api/dto/ProfileImage.kt
@@ -0,0 +1,13 @@
+package com.example.imagesearch.api.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProfileImage(
+    @SerializedName("large")
+    val large: String,
+    @SerializedName("medium")
+    val medium: String,
+    @SerializedName("small")
+    val small: String
+)
