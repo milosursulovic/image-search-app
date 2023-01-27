ba3f3b59427d5b7594756f464d4844c1ab1e86e2
Misa <milosursulovic2696@gmail.com>
2023-01-27 19:51:22 +0100

    Add layout for list item view

--- app/src/main/java/com/example/imagesearch/MainActivity.kt
+++ app/src/main/java/com/example/imagesearch/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.imagesearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    //https://unsplash.com/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
