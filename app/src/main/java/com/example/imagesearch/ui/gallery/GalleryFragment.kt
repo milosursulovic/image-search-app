b0516a4986582343d468738696b402a7ed3bdc03
Misa <milosursulovic2696@gmail.com>
2023-02-08 15:47:35 +0100

    Add paging source and data as flow

--- app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,22 @@
 package com.example.imagesearch.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.imagesearch.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+    private val viewModel: GalleryViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.subscribeToState { state ->
+            if (!state.isLoading) {
+                state.pagingData
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/imagesearch/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/imagesearch/ui/gallery/GalleryViewModel.kt
@@ -1,12 +1,52 @@
 package com.example.imagesearch.ui.gallery
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.example.imagesearch.data.UnsplashPhoto
 import com.example.imagesearch.data.UnsplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(private val repository: UnsplashRepository) :
     ViewModel() {
 
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+    }
+
+    private val currentQuery = MutableStateFlow(DEFAULT_QUERY)
+    private var photos: PagingData<UnsplashPhoto>? = null
+    private var state = PagingDataState()
+
+    init {
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            currentQuery.collect { query ->
+                repository.getSearchResults(query).collect { pagingData ->
+                    photos = pagingData
+                    state = state.copy(
+                        isLoading = false,
+                        pagingData = photos
+                    )
+                }
+            }
+        }
+    }
+
+    fun subscribeToState(callback: (PagingDataState) -> Unit) {
+        callback(state)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
 }
+
+data class PagingDataState(
+    val isLoading: Boolean = false,
+    val pagingData: PagingData<UnsplashPhoto>? = null
+)
