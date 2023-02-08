677471cc80e39e1db8e1bc0763cfbc052fef4d8d
Misa <milosursulovic2696@gmail.com>
2023-02-08 16:22:31 +0100

    Implement paging data adapter

--- app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
@@ -5,18 +5,34 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.imagesearch.R
+import com.example.imagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel: GalleryViewModel by viewModels()
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentGalleryBinding.bind(view)
+        val adapter = UnsplashPhotoAdapter()
+        binding.apply {
+            rvGallery.setHasFixedSize(true)
+            rvGallery.adapter = adapter
+        }
         viewModel.subscribeToState { state ->
             if (!state.isLoading) {
-                state.pagingData
+                state.pagingData?.let {
+                    adapter.submitData(viewLifecycleOwner.lifecycle, it)
+                }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
